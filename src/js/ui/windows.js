65707612ce6083233c9e0cf5b2a2fa32453dc988
Strawberry 3 <strawberry-development@protonmail.com>
2024-09-18 21:42:10 +0200

    small visual fix, nothing much

--- src/js/ui/windows.js
+++ src/js/ui/windows.js
@@ -4,10 +4,10 @@ function toggleContent(element) {
 
     if (content.style.display === 'block' || content.style.display === '') {
         content.style.display = 'none';
-        icon.textContent = '+';
+        icon.innerHTML = '<i class="fa fa-angle-up" aria-hidden="true"></i>';
     } else {
         content.style.display = 'block';
-        icon.textContent = '-';
+        icon.innerHTML = '<i class="fa fa-angle-down" aria-hidden="true"></i>';
     }
 
     content.classList.toggle('hidden-content');
